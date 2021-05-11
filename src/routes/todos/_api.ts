8c869a0831f89d8d165185718741281ba2149f3c
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-11 10:33:11 +0200

    Update Prettier config, lint & format files

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -1,5 +1,5 @@
-import type { Request } from '@sveltejs/kit';
-import type { Locals } from '$lib/types';
+import type { Request } from "@sveltejs/kit";
+import type { Locals } from "$lib/types";
 
 /*
 	This module is used by the /todos.json and /todos/[uid].json
@@ -12,37 +12,37 @@ import type { Locals } from '$lib/types';
 	guarantees are made. Don't use it to organise your life.)
 */
 
-const base = 'https://api.svelte.dev';
+const base = "https://api.svelte.dev";
 
 export async function api(request: Request<Locals>, resource: string, data?: {}) {
-	// user must have a cookie set
-	if (!request.locals.userid) {
-		return { status: 401 };
-	}
+  // user must have a cookie set
+  if (!request.locals.userid) {
+    return { status: 401 };
+  }
 
-	const res = await fetch(`${base}/${resource}`, {
-		method: request.method,
-		headers: {
-			'content-type': 'application/json'
-		},
-		body: data && JSON.stringify(data)
-	});
+  const res = await fetch(`${base}/${resource}`, {
+    method: request.method,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: data && JSON.stringify(data),
+  });
 
-	// if the request came from a <form> submission, the browser's default
-	// behaviour is to show the URL corresponding to the form's "action"
-	// attribute. in those cases, we want to redirect them back to the
-	// /todos page, rather than showing the response
-	if (res.ok && request.method !== 'GET' && request.headers.accept !== 'application/json') {
-		return {
-			status: 303,
-			headers: {
-				location: '/todos'
-			}
-		};
-	}
+  // if the request came from a <form> submission, the browser's default
+  // behaviour is to show the URL corresponding to the form's "action"
+  // attribute. in those cases, we want to redirect them back to the
+  // /todos page, rather than showing the response
+  if (res.ok && request.method !== "GET" && request.headers.accept !== "application/json") {
+    return {
+      status: 303,
+      headers: {
+        location: "/todos",
+      },
+    };
+  }
 
-	return {
-		status: res.status,
-		body: await res.json()
-	};
+  return {
+    status: res.status,
+    body: await res.json(),
+  };
 }
