3c06174bfebbb5c699c2a81e03b4c4c76da9ff86
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-14 18:44:17 +0200

    Add inputs to submit screen and button

--- src/routes/submit.svelte
+++ src/routes/submit.svelte
@@ -4,12 +4,45 @@
   import ModalContent from "$lib/Modal/ModalContent.svelte";
   import ModalFooter from "$lib/Modal/ModalFooter.svelte";
   import ModalOverlay from "$lib/Modal/ModalOverlay.svelte";
+  import marked from "marked";
+  import clsx from "clsx";
 
   export const hydrate = true;
 </script>
 
 <script lang="ts">
+  let source = `
+# H1 heading
+
+## H2 heading
+
+### H3 heading
+
+--------
+
+**bold text**
+
+*italicized text*
+
+--------
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+[Svelte](https://svelte.dev/)
+`;
+  let markdown = marked(source);
+
   let showModal = false;
+
+  function handleSubmit() {
+    console.log(source);
+  }
 </script>
 
 <svelte:head>
@@ -17,19 +50,81 @@
 </svelte:head>
 
 <div class="content">
-  <div class="flex flex-col">
+  <form on:submit|preventDefault={handleSubmit}>
     <h1 class="text-2xl mb-4">Submit a new Company</h1>
 
-    <input type="text" placeholder="Company Name" class="input mb-4" />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+      <input type="text" placeholder="Company Name" class="input" />
 
-    <input
-      type="text"
-      placeholder="Parent Company"
-      class="input"
-      readonly
-      on:click={() => (showModal = true)}
-    />
-  </div>
+      <input
+        type="text"
+        placeholder="Parent Company"
+        class="input"
+        readonly
+        on:click={() => (showModal = true)}
+      />
+    </div>
+
+    <div class="flex w-full mb-8">
+      <div class="flex-1 flex flex-col">
+        <p class="font-bold mb-2">Reason</p>
+        <textarea
+          bind:value={source}
+          class="flex-grow border input text-gray-800 p-4 focus:outline-none"
+        />
+      </div>
+
+      <div class="w-4" />
+
+      <div class="flex-1">
+        <p class="font-bold mb-2">Preview</p>
+        <div class="px-4">{@html markdown}</div>
+      </div>
+    </div>
+
+    <p class="mb-4 font-bold">Please provide us with a source.</p>
+    <input type="url" placeholder="Source" class="input mb-4" />
+    <div class="text-gray-600 border-l-2 border-gray-300 pl-2 text-sm flex mb-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 mr-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>
+        A source allows us to verify the information is accurate and make more informed decisions
+        when purchasing from a company.
+      </span>
+    </div>
+
+    <div class="flex flex-row-reverse">
+      <button
+        type="submit"
+        class={clsx(
+          "bg-gradient-to-r",
+          "from-red-500",
+          "to-yellow-500",
+          "hover:ring-2",
+          "hover:ring-orange-500",
+          "transition-shadow",
+          "rounded",
+          "px-4",
+          "py-2",
+          "text-white",
+        )}
+      >
+        Submit
+      </button>
+    </div>
+  </form>
 
   <Modal open={showModal} on:onClose={() => (showModal = false)}>
     <ModalOverlay />
