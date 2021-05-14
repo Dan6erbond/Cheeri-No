3b847ce2fbe06858feb2857214467808a0dbddfe
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-14 19:46:57 +0200

    Add company resource page with reasons list

---
+++ src/lib/Reason/index.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import clsx from "clsx";
+</script>
+
+<div
+  class={clsx(
+    "shadow-lg",
+    "mb-6",
+    "transform",
+    "hover:-translate-y-1",
+    "transition-transform",
+    "rounded-md",
+    "overflow-hidden",
+  )}
+>
+  <div class="flex bg-white">
+    <div class="flex flex-col flex-grow">
+      <div class="p-4">
+        <figure>
+          <figcaption class="text-sm mb-2">Anonymous, 13.05.2021</figcaption>
+          <blockquote class="border-l-2 pl-2 border-gray-100 italic mb-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus ducimus nisi,
+            voluptas iusto temporibus rerum a optio minima ratione cumque reiciendis numquam id
+            modi? Repudiandae earum odit quas totam laborum!
+          </blockquote>
+        </figure>
+      </div>
+      <div class="px-4 py-2 bg-gray-50 flex items-center">
+        <span>10</span>
+        <span class="mx-2">|</span>
+        <button class="hover:text-red-400 transition-colors hover:animate-bounce">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
+            />
+          </svg>
+        </button>
+        <span class="mx-2">|</span>
+        <button class="hover:text-red-400 transition-colors hover:animate-bounce">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
+            />
+          </svg>
+        </button>
+        <div class="flex-grow" />
+        <div class="flex items-center">
+          <span class="mr-2"><span class="font-bold">Source:</span> wikipedia.org</span>
+          <a href="#" target="_blank" class="text-blue-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+              />
+              <path
+                d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/companies/[id].svelte
@@ -0,0 +1,85 @@
+<script context="module">
+  import Reason from "$lib/Reason/index.svelte";
+  import clsx from "clsx";
+
+  export const hydrate = true;
+</script>
+
+<script lang="ts">
+  let sort: "votes" | "new" = "votes";
+</script>
+
+<h1 class="text-2xl mb-4">Nestlé</h1>
+
+<p class="text-lg">Reasons</p>
+
+<div class="flex w-full mb-2 px-2 py-4 items-center">
+  <button
+    class={clsx("flex", "mr-4", sort !== "votes" && "hover:text-gray-500", "transition-colors")}
+    on:click={() => (sort = "votes")}
+    class:text-orange-400={sort === "votes"}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 mr-2"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+      />
+    </svg>
+    <span>Votes</span>
+  </button>
+  <button
+    class={clsx("flex", sort !== "new" && "hover:text-gray-500", "transition-colors")}
+    on:click={() => (sort = "new")}
+    class:text-orange-400={sort === "new"}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 mr-2"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <span>New</span>
+  </button>
+  <div class="flex-grow" />
+  <button
+    class="flex items-center hover:no-underline hover:bg-orange-50 transition-colors p-2 rounded"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 mr-2"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      />
+    </svg>
+    <span class="mr-2 hidden md:inline">Add a Reason</span>
+  </button>
+</div>
+
+<div class="flex flex-col -mb-6">
+  {#each Array(10) as _, idx (idx)}
+    <Reason />
+  {/each}
+</div>
