8757fe7b436ace2640b11b259c4334ca172e5c1c
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-14 08:27:01 +0200

    Create `CompanyCard` component with dummy data
    Remove image placeholder.

---
+++ src/lib/Company/CompanyCard.svelte
@@ -0,0 +1,75 @@
+<div
+  class="shadow-lg mb-6 transform hover:-translate-y-1 transition-transform bg-white rounded-md overflow-hidden"
+>
+  <div class="flex">
+    <div class="flex flex-col flex-grow">
+      <div class="p-10">
+        <h3 class="text-xl mb-4">Nestlé</h3>
+        <div class="text-gray-400 text-sm">
+          <p class="font-bold mb-2">Top reason:</p>
+          <figure>
+            <blockquote class="border-l-2 pl-2 border-gray-100 italic mb-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus ducimus nisi,
+              voluptas iusto temporibus rerum a optio minima ratione cumque reiciendis numquam id
+              modi? Repudiandae earum odit quas totam laborum!
+            </blockquote>
+            <figcaption>Anonymous, 13.05.2021</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="px-10 py-5 bg-gray-50 flex">
+        <div class="flex items-center">
+          <button class="hover:text-red-400 transition-colors hover:animate-bounce">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
+              />
+            </svg>
+          </button>
+          <span class="mx-2">|</span>
+          <button class="hover:text-red-400 transition-colors hover:animate-bounce">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="flex-grow" />
+        <a href="/companies/id" class="flex items-center">
+          <span>View</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import CompanyCard from "$lib/Company/CompanyCard.svelte";
+
   let view: "list" | "card" = "list";
   let sort: "votes" | "new" = "votes";
 </script>
@@ -30,11 +32,7 @@
       </svg>
       <span>Votes</span>
     </button>
-    <button
-      class="flex"
-      on:click={() => (sort = "new")}
-      class:text-orange-400={sort === "new"}
-    >
+    <button class="flex" on:click={() => (sort = "new")} class:text-orange-400={sort === "new"}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-6 w-6 mr-2"
@@ -88,153 +86,13 @@
   {#if view === "list"}
     <div class="flex flex-col -mb-6 justify-items-stretch w-full">
       {#each Array(10) as _, idx (idx)}
-        <div
-          class="shadow-lg mb-6 transform hover:-translate-y-1 transition-transform bg-white rounded-md overflow-hidden"
-        >
-          <div class="flex">
-            <div>
-              <img
-                class="w-40 h-full object-cover"
-                src="https://via.placeholder.com/200x150"
-                alt=""
-              />
-            </div>
-            <div class="flex flex-col flex-grow">
-              <div class="p-10">
-                <h3 class="text-xl">Nestlé</h3>
-              </div>
-              <div class="px-10 py-5 bg-gray-50 flex">
-                <div class="flex items-center">
-                  <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
-                      />
-                    </svg>
-                  </button>
-                  <span class="mx-2">|</span>
-                  <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div class="flex-grow" />
-                <a href="/companies/id" class="flex items-center">
-                  <span>View</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CompanyCard />
       {/each}
     </div>
   {:else}
     <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
       {#each Array(10) as _, idx (idx)}
-        <div
-          class="shadow-lg transform hover:-translate-y-1 transition-transform bg-white rounded-md overflow-hidden"
-        >
-          <div class="flex flex-col">
-            <div>
-              <img
-                class="h-36 w-full object-cover"
-                src="https://via.placeholder.com/200x150"
-                alt=""
-              />
-            </div>
-            <div class="flex flex-col flex-grow">
-              <div class="p-6">
-                <h3 class="text-xl">Nestlé</h3>
-              </div>
-              <div class="px-6 py-5 bg-gray-50 flex">
-                <div class="flex items-center">
-                  <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
-                      />
-                    </svg>
-                  </button>
-                  <span class="mx-2">|</span>
-                  <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div class="flex-grow" />
-                <a href="/companies/id" class="flex items-center">
-                  <span>View</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CompanyCard />
       {/each}
     </div>
   {/if}
