e1851fbec44e89e6e867cfc422dd180739aca29d
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-14 19:46:25 +0200

    Remove vote buttons from company card

--- src/lib/Company/CompanyCard.svelte
+++ src/lib/Company/CompanyCard.svelte
@@ -30,41 +30,6 @@
         </div>
       </div>
       <div class="px-10 py-5 bg-gray-50 flex">
-        <div class="flex items-center">
-          <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
-              />
-            </svg>
-          </button>
-          <span class="mx-2">|</span>
-          <button class="hover:text-red-400 transition-colors hover:animate-bounce">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"
-              />
-            </svg>
-          </button>
-        </div>
         <div class="flex-grow" />
         <a href="/companies/id" class="flex items-center">
           <span>View</span>
