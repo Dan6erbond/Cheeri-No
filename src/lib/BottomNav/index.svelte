6c72b24fae4d8523d96edce6b71b39b646c7a6fb
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-15 14:18:11 +0200

    Shrink bottom nav and switch out icons on active path

--- src/lib/BottomNav/index.svelte
+++ src/lib/BottomNav/index.svelte
@@ -1,28 +1,65 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import clsx from "clsx";
 </script>
 
 <div
-  class="flex bg-white border-t border-gray-300 left-0 bottom-0 right-0 fixed p-4 transition-transform transform md:translate-y-56 justify-center"
+  class={clsx(
+    "flex",
+    "bg-white",
+    "border-t",
+    "border-gray-300",
+    "left-0",
+    "bottom-0",
+    "right-0",
+    "fixed",
+    "p-2",
+    "transition-transform",
+    "transform md:translate-y-56",
+    "justify-center",
+  )}
 >
   <a
     href="/submit"
-    class="flex items-center justify-center hover:no-underline hover:bg-orange-50 transition-colors p-2 rounded-full"
-    class:text-orange-400={$page.path === "/submit"}
+    class={clsx(
+      "flex",
+      "items-center",
+      "justify-center",
+      "hover:no-underline",
+      "hover:bg-orange-50",
+      "transition-colors",
+      "p-2",
+      "rounded-full",
+    )}
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-      />
-    </svg>
+    {#if $page.path === "/submit"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    {:else}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    {/if}
   </a>
 </div>
