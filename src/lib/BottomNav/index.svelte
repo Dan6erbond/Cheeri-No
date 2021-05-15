765c430243b7547d08e83622ce835279d973887c
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-15 14:39:48 +0200

    Add additional nav icons for home / login

--- src/lib/BottomNav/index.svelte
+++ src/lib/BottomNav/index.svelte
@@ -16,9 +16,50 @@
     "p-2",
     "transition-transform",
     "transform md:translate-y-56",
-    "justify-center",
+    "justify-evenly",
   )}
 >
+  <a
+    href="/"
+    class={clsx(
+      "flex",
+      "items-center",
+      "justify-center",
+      "hover:no-underline",
+      "hover:bg-orange-50",
+      "transition-colors",
+      "p-2",
+      "rounded-full",
+    )}
+  >
+    {#if $page.path === "/"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+        />
+      </svg>
+    {:else}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    {/if}
+  </a>
   <a
     href="/submit"
     class={clsx(
@@ -62,4 +103,93 @@
       </svg>
     {/if}
   </a>
+  {#if true}
+    <a
+      href="/profile"
+      class={clsx(
+        "flex",
+        "items-center",
+        "justify-center",
+        "hover:no-underline",
+        "hover:bg-orange-50",
+        "transition-colors",
+        "p-2",
+        "rounded-full",
+      )}
+    >
+      {#if $page.path === "/profile"}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        </svg>
+      {/if}
+    </a>
+  {:else}
+    <a
+      href="/login"
+      class={clsx(
+        "flex",
+        "items-center",
+        "justify-center",
+        "hover:no-underline",
+        "hover:bg-orange-50",
+        "transition-colors",
+        "p-2",
+        "rounded-full",
+      )}
+    >
+      {#if $page.path === "/login"}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+          />
+        </svg>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      {/if}
+    </a>
+  {/if}
 </div>
