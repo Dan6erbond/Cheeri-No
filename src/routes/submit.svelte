9d5ada23d8abf528e4280bf1bb0d671152a9d9b8
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-15 10:45:01 +0200

    Make input clearable

--- src/routes/submit.svelte
+++ src/routes/submit.svelte
@@ -56,13 +56,29 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
       <input type="text" placeholder="Company Name" class="input" />
 
-      <input
-        type="text"
-        placeholder="Parent Company"
-        class="input"
-        readonly
-        on:click={() => (showModal = true)}
-      />
+      <div class="input flex items-center">
+        <input
+          type="text"
+          placeholder="Parent Company"
+          readonly
+          class="focus:outline-none flex-grow"
+          on:click={() => (showModal = true)}
+        />
+        <button class="text-gray-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
 
     <div class="flex w-full mb-8">
