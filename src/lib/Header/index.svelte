e92e22fb0ae063c99850aa5f7fcafdc76596c6f1
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-17 17:26:54 +0200

    Add log out button

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { page } from "$app/stores";
+  import { page, session } from "$app/stores";
+  import { signOut } from "$lib/SvelteAuth/client";
   import clsx from "clsx";
   import logo from "./logo.svg";
-  import { session } from "$app/stores";
 </script>
 
 <header
@@ -63,7 +63,7 @@
 
   <div class="flex-grow" />
 
-  <div class="flex items-center justify-center">
+  <div class="flex items-center justify-center space-x-2">
     {#if $session?.user}
       <a
         href="/profile"
@@ -108,6 +108,34 @@
           </svg>
         {/if}
       </a>
+      <button
+        class={clsx(
+          "flex",
+          "items-center",
+          "justify-center",
+          "hover:no-underline",
+          "hover:bg-orange-50",
+          "transition-colors",
+          "p-2",
+          "rounded-full",
+        )}
+        on:click={signOut}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+          />
+        </svg>
+      </button>
     {:else}
       <a
         href="/login"
